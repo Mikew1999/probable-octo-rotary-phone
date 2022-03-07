0ef36e6220b0f5075acb9850f89005882bdc7ef0
Mikew1999 <mikewalters1234@gmail.com>
2022-03-07 19:00:26 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+''' Main URL routing '''
+import os
+
+# Imports required modules from Flask
+from flask import (
+    Flask, url_for,
+    request, flash,
+    redirect, render_template)
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("SECRET_KEY")
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
